0cc0abefc8bb8353fca42270bff41009e7e0e094
IamNotG37 <g37.apple@gmail.com>
2024-09-11 18:11:55 +0900

    Delete server directory

--- server/chunks/0-iZQLOl38.js
+++
@@ -1,9 +0,0 @@
-const index = 0;
-let component_cache;
-const component = async () => component_cache ??= (await import('./layout.svelte-BGQbCkpn.js')).default;
-const imports = ["_app/immutable/nodes/0.Xz-cbj-B.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.OsuhnpW-.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=0-iZQLOl38.js.map
